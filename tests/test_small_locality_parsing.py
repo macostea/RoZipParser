769d7ab4fd74be537117d975fc45640a6e309da5
Mihai Costea <mihai.andrei.costea@icloud.com>
2016-09-19 11:29:35 +0300

    Add tests

---
+++ tests/test_small_locality_parsing.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+
+import unittest
+from codeparser import CodeParser
+
+
+class TestSmallLocalityParsing(unittest.TestCase):
+    def test_number_of_codes(self):
+        parser = CodeParser("tests/inputs/small_locality_input.xlsx")
+        codes = parser.get_codes()
+
+        self.assertEqual(len(codes), 9)
+
+    def test_code_correctness(self):
+        parser = CodeParser("tests/inputs/small_locality_input.xlsx")
+        codes = parser.get_codes()
+
+        first_code = codes[0]
+        self.assertEqual(first_code.county, u"Ilfov")
+        self.assertEqual(first_code.locality, u"Buftea")
+        self.assertIsNone(first_code.sector)
+        self.assertIsNone(first_code.street)
+        self.assertIsNone(first_code.house_number)
+        self.assertEqual(first_code.zip, u"070000")
+        self.assertIsNone(first_code.street_type)
